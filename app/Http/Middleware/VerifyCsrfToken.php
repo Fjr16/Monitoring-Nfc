e51f476b09a257427095416d4c5a8d6c9c5fb7e4
Fjr16 <fajri.fadhila@gmail.com>
2024-02-23 04:44:30 +0700

    update except endpoint csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'register/card',
+        'register/card/*',
         'login/card',
         'login/pin',
     ];
